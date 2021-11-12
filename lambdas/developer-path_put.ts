21bc26e51650e16d487795a9d4df24ee74d21ed6
David Vargas <dvargas92495@gmail.com>
2021-11-12 17:47:03 -0500

    fix logic

--- lambdas/developer-path_put.ts
+++ lambdas/developer-path_put.ts
@@ -166,7 +166,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       headers,
     };
   }
-  if (premium && (!stripeAccountId || email !== "dvargas92495@gmail.com")) {
+  if (premium && !(stripeAccountId || email === "dvargas92495@gmail.com")) {
     return {
       statusCode: 403,
       body: "Need to connect a stripe account id before publishing premium features for an extension",
