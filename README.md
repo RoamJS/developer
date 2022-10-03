7bf3c3573da519640a991e8cc8e508d5f7107f5c
David Vargas <dvargas92495@gmail.com>
2022-10-03 01:03:13 -0400

    Clean out developer and prepare for Roam Depot

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# developer
+# Developer
       
-Develop and publish your own Roam extensions directly to RoamJS!      
+Develop and publish your own Roam extensions with the help of RoamJS!
+
+## Usage
+The Developer extension on RoamJS helps other software engineers develop and share extensions with other Roam users! It provides features for developing from within Roam as well as exposing methods from RoamJS' official [npm package](https://npmjs.com/roamjs-components).
+
+## Developing within Roam
+To get started developing within Roam, simply enter three back ticks into a block. Roam natively will render a code editor where you can start writing your logic.
+
+![](https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o/imgs%2Fapp%2Froamjs%2FcpGj9vg1c2.00.17%20PM.png?alt=media&token=ced7a892-2ec5-419f-9d2b-93b07911b242)
+
+To easily invoke this logic without having to reload Roam each time, create a button in Roam with the following syntax: `{{name:developer:uid}}`. Clicking this button will look for a code block within the block referenced by `uid` and invoke the logic contained within it. `name` could be any text value that doesn't contain a colon that you want.
+
+![](https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o/imgs%2Fapp%2Froamjs%2FdFr9G-GW1f.00.59%20PM.png?alt=media&token=b7515150-a650-40d2-b608-1ee88b32d849)
+
+![](https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o/imgs%2Fapp%2Froamjs%2FPjrj8kZex0.01.09%20PM.png?alt=media&token=a00101b0-0d22-48bc-bc6f-6dc1b2b3bb2f)
+
+## Developing outside of Roam
+RoamJS manages two NPM packages, both of which have been vetted by the Roam team themselves:
+- [RoamJS Components](https://roamjs.com/extensions/developer/roamjs_components) - This package contains all of the UI components and utilities RoamJS uses to build extensions
+- [RoamJS Scripts](https://roamjs.com/extensions/developer/roamjs_scripts) - This package contains all of the build scripts and command line utilities RoamJS uses to build extensions
+
+These packages are what all RoamJS extensions are built on top of. As a developer, you are not required to use either of these extensions, but they exist to make your life easier getting started.
+
+## Resources
+Most existing developer documentation lives at [developer.roamjs.com](https://developer.roamjs.com). Stay tuned while we migrate that information to RoamJS.
--- src/index.ts
+++ src/index.ts
@@ -1,87 +1,18 @@
-import Dashboard from "./DeveloperDashboard";
-import addStyle from "roamjs-components/dom/addStyle";
 import runExtension from "roamjs-components/util/runExtension";
-import toConfigPageName from "roamjs-components/util/toConfigPageName";
-import {
-  createConfigObserver,
-  render as configPageRender,
-} from "roamjs-components/components/ConfigPage";
-import type {
-  Field,
-  CustomField,
-} from "roamjs-components/components/ConfigPanels/types";
-import CustomPanel from "roamjs-components/components/ConfigPanels/CustomPanel";
-import BlocksPanel from "roamjs-components/components/ConfigPanels/BlocksPanel";
-import TextPanel from "roamjs-components/components/ConfigPanels/TextPanel";
-import MultiTextPanel from "roamjs-components/components/ConfigPanels/MultiTextPanel";
 import createHTMLObserver from "roamjs-components/dom/createHTMLObserver";
 import getBlockUidFromTarget from "roamjs-components/dom/getBlockUidFromTarget";
-import StripePanel from "./StripePanel";
 import getTextByBlockUid from "roamjs-components/queries/getTextByBlockUid";
 import createBlock from "roamjs-components/writes/createBlock";
 import extractRef from "roamjs-components/util/extractRef";
-import getPageTitleValueByHtmlElement from "roamjs-components/dom/getPageTitleValueByHtmlElement";
-import { getExtensions, hasPath } from "./pathsCache";
-import React from "react";
-import ExtensionPage from "./ExtensionsPage";
 import getCodeFromBlock from "./getCodeFromBlock";
 
-const extensionId = "developer";
 const AsyncFunction: FunctionConstructor = new Function(
   `return Object.getPrototypeOf(async function(){}).constructor`
 )();
 
 export default runExtension({
-  extensionId,
   run: async () => {
-    const style = addStyle(`.roamjs-developer-path {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding-left: 16px;
-      border-radius: 4px;
-    }
-    
-    .roamjs-developer-path:hover {
-      background-color: #eeeeee;
-    }`);
-    const { observer } = await createConfigObserver({
-      title: toConfigPageName(extensionId),
-      config: {
-        tabs: [
-          {
-            id: "home",
-            fields: [
-              {
-                Panel: CustomPanel,
-                title: "extensions",
-                description: "The list of extensions you have on RoamJS",
-                options: {
-                  component: Dashboard,
-                },
-              } as Field<CustomField>,
-              {
-                Panel: TextPanel,
-                title: "prefix",
-                description:
-                  "The prefix used for the documentation for your extensions",
-              },
-              {
-                Panel: CustomPanel,
-                title: "stripe",
-                description:
-                  "Connect your Stripe account to receive sponsorships directly",
-                options: {
-                  component: StripePanel,
-                },
-              } as Field<CustomField>,
-            ],
-          },
-        ],
-      },
-    });
-
-    const buttonObserver = createHTMLObserver({
+    createHTMLObserver({
       className: "bp3-button",
       tag: "BUTTON",
       callback: (b: HTMLButtonElement) => {
@@ -135,81 +66,5 @@ export default runExtension({
         }
       },
     });
-
-    const headingObserver = createHTMLObserver({
-      tag: "H1",
-      className: "rm-title-display",
-      callback: (h1: HTMLHeadingElement) => {
-        const title = getPageTitleValueByHtmlElement(h1);
-        if (hasPath(title)) {
-          const extension = getExtensions().find((e) => title.endsWith(e.id));
-          configPageRender({
-            h: h1,
-            title,
-            config: {
-              tabs: [
-                {
-                  id: "home",
-                  fields: [
-                    {
-                      title: "Actions",
-                      description:
-                        "Publish or delete your extension from RoamJS",
-                      Panel: CustomPanel,
-                      options: {
-                        component: ({ parentUid }) =>
-                          React.createElement(ExtensionPage, {
-                            title,
-                            parentUid,
-                            ...extension,
-                          }),
-                      },
-                    } as Field<CustomField>,
-                    {
-                      title: "Documentation",
-                      description:
-                        "The content users need to know how to use your extension",
-                      Panel: BlocksPanel,
-                    },
-                    {
-                      title: "Description",
-                      description: "The description for your extension",
-                      Panel: TextPanel,
-                    },
-                    {
-                      title: "Contributors",
-                      description:
-                        "Anyone else who helped contribute to the extension",
-                      Panel: MultiTextPanel,
-                    },
-                    {
-                      title: "Thumbnail",
-                      description: "The thumbnail to represent your extension",
-                      Panel: TextPanel,
-                    },
-                    {
-                      title: "Entry",
-                      description:
-                        "The custom URL to host the main entry file of your extension, if not hosted on RoamJS",
-                      Panel: TextPanel,
-                    },
-                    {
-                      title: "Implementation",
-                      description:
-                        "Block reference pointing to the implementation of the extension, if coded within Roam",
-                      Panel: TextPanel,
-                    },
-                  ],
-                },
-              ],
-            },
-          });
-        }
-      },
-    });
-    return {
-      elements: [style],
-      observers: [observer, buttonObserver, headingObserver],
-    };
   },
 });
